b595cd606373744087c10dd2ecf0f0965a2a8036
user <ilginngnc@gmail.com>
2025-12-06 21:42:51 +0300

    güncellendi

--- src/ceng201/hw/TreatmentQueue.java
+++ src/ceng201/hw/TreatmentQueue.java
@@ -1,8 +1,10 @@
 package ceng201.hw;
 
 public class TreatmentQueue {
-    private Node head;
-    private Node tail;
+    private Node front;
+    private Node rear;
+    private int size;
+
 
     private class Node {
         TreatmentRequest data;
@@ -16,13 +18,39 @@ public class TreatmentQueue {
     public void enqueue(TreatmentRequest request) {
         Node node = new Node(request);
 
-        if (head==null){
-            head=node;
-            tail=node;
+        if (front == null) {
+            front = node;
+            rear = node;
             System.out.println("First patient added.");
+            size++;
+        } else { //I added patient at the end.
+            node.next = null;
+            rear.next = node;
+            rear = node;
+            size++;
         }
-        else{
+    }
 
+    public TreatmentRequest dequeue() { //delete first patient
+        if (front == null) {
+            System.out.println("The list is empty.");
+            return null;
+        } else if (front.next == null) {
+            TreatmentRequest current = front.data;
+            front = null;
+            rear = null;
+            System.out.println("The only patient deleted.");
+            size--;
+            return current;
+        } else {
+            TreatmentRequest current = front.data;
+            front = front.next;
+            size--;
+            return current;
         }
+
+    }
+    public int size(){
+        return size;
     }
 }
