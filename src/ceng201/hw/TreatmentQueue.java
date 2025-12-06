b4c326451ec3561dfa4a1749bc1aa25c800db9ed
user <ilginngnc@gmail.com>
2025-12-06 14:45:05 +0300

    güncellendi

--- src/ceng201/hw/TreatmentQueue.java
+++ src/ceng201/hw/TreatmentQueue.java
@@ -1,4 +1,28 @@
 package ceng201.hw;
 
 public class TreatmentQueue {
+    private Node head;
+    private Node tail;
+
+    private class Node {
+        TreatmentRequest data;
+        Node next;
+
+        Node(TreatmentRequest data) {
+            this.data = data;
+        }
+    }
+
+    public void enqueue(TreatmentRequest request) {
+        Node node = new Node(request);
+
+        if (head==null){
+            head=node;
+            tail=node;
+            System.out.println("First patient added.");
+        }
+        else{
+
+        }
+    }
 }
