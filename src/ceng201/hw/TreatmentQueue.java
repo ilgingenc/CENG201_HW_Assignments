8bce055239eb34948aadd94066777700af7c4822
user <ilginngnc@gmail.com>
2025-12-08 20:00:16 +0300

    güncellendi

--- src/ceng201/hw/TreatmentQueue.java
+++ src/ceng201/hw/TreatmentQueue.java
@@ -10,7 +10,7 @@ public class TreatmentQueue {
         TreatmentRequest treatmentRequest;
         Node next;
 
-        Node(TreatmentRequest data) {
+        Node(TreatmentRequest data) { //constructor
             this.treatmentRequest = data;
         }
     }
@@ -18,35 +18,39 @@ public class TreatmentQueue {
     public void enqueue(TreatmentRequest request) {
         Node node = new Node(request);
 
-        if (front == null) {
+        if (front == null) { //If queue is empty.
             front = node;
             rear = node;
-            System.out.println("First patient added.");
+            System.out.println("First patient added. Added pattient:"+ request.patientId);
             size++;
         } else { //I added patient at the end.
             node.next = null;
             rear.next = node;
             rear = node;
             size++;
+            System.out.println("Added patient: " + request.patientId);
         }
     }
 
     public TreatmentRequest dequeue() { //delete first patient
-        if (front == null) {
+        if (front == null) { //If the list is empty.
             System.out.println("The list is empty.");
             return null;
-        } else if (front.next == null) {
+        } else if (front.next == null) { //If there is just one patient.
             TreatmentRequest current = front.treatmentRequest;
             front = null;
             rear = null;
             System.out.println("The only patient deleted.");
             size--;
             return current;
-        } else {
+        } else { // If there are more than one element
             TreatmentRequest current = front.treatmentRequest;
             front = front.next;
             size--;
+            System.out.println("Patient removed from queue: " + current.patientId);
+
             return current;
+
         }
 
     }
@@ -55,11 +59,12 @@ public class TreatmentQueue {
         return size;
     }
 
-    public void print() {
+    public void print() { //Prints all patient IDs in the queue.
+        System.out.println("Queue:");
         Node temp;
         temp=front;
         while (temp!=null){
-            System.out.println(temp.treatmentRequest.patientId);
+            System.out.print(temp.treatmentRequest.patientId + " ");
             temp= temp.next;
         }
 
