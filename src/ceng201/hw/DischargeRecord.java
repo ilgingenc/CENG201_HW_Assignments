7f7b1d2ddd05a4fd8ff8530efa9b981649a77dbf
user <ilginngnc@gmail.com>
2025-12-09 15:15:38 +0300

    güncellendi

--- src/ceng201/hw/DischargeRecord.java
+++ src/ceng201/hw/DischargeRecord.java
@@ -1,6 +1,7 @@
 package ceng201.hw;
 
-public class DischargeRecord { int patientId;
+public class DischargeRecord {
+    int patientId;
     long dischargeTime;
 
     public DischargeRecord(int patientId, long dischargeTime){
--- src/ceng201/hw/DischargeStack.java
+++ src/ceng201/hw/DischargeStack.java
@@ -2,7 +2,7 @@ package ceng201.hw;
 
 public class DischargeStack {
     private Node top;
-    private int size;
+
 
     private class Node {
 
@@ -13,11 +13,49 @@ public class DischargeStack {
             this.x = data;
         }
     }
+
     public void push(DischargeRecord r) { //adding elements to top.
         Node node = new Node(r);//the object was created.
         node.next = top;
         top = node;
-        System.out.println("Patient "+ r.patientId +"added to stack.");
+        System.out.println("Patient " + r.patientId + "added to stack.");
     }
+
+    public DischargeRecord pop() {
+        if (top == null) {
+            System.out.println("The stack is empty.");
+            return null;
+
+        }
+        DischargeRecord deleted = top.x;
+        top = top.next;
+        return deleted;
     }
+
+    public DischargeRecord peek() {
+        if (top == null) {
+            System.out.println("The stack is empty.");
+            return null;
+        }
+        DischargeRecord first = top.x;
+        return first;
+
+    }
+
+    public void printStack() {
+        if (top == null) {
+            System.out.println("The stack is empty.");
+            return;
+        }
+        Node temp = top;
+        while (top != null) {
+            System.out.println("Patient id: " + temp.x.patientId + "Patient Discharge Time: " + temp.x.dischargeTime);
+
+            top = top.next;
+
+
+        }
+    }
+}
+
 
